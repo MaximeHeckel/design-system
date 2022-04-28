ff98cdd3f9bfd49084965983270e2265edf868ba
Maxime <heckelmaxime@gmail.com>
2022-04-28 19:38:10 -0400

    feat(component): Add Card component

---
+++ src/lib/utils/isElementOfType.ts
@@ -0,0 +1,15 @@
+/**
+ * Returns true if the element is of type P, returns false otherwise
+ * @param {any} element
+ * @param {React.ComponentType<P>} ComponentType
+ * @returns {boolean}
+ */
+export function isElementOfType<P = {}>(
+  element: unknown,
+  ComponentType: React.ComponentType<P>
+): element is React.ReactElement<P> {
+  const reactElement = element as React.ReactElement;
+
+  // @ts-ignore ts complains about displayName not existing on `type`
+  return reactElement?.type?.displayName === ComponentType.displayName;
+}
