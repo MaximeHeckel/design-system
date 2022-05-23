d770a4671298492300650306fd8183c399c9843f
Maxime <heckelmaxime@gmail.com>
2022-05-22 22:30:23 -0400

    chore(format): Run prettier on esbuild dev config

--- esbuild.dev.js
+++ esbuild.dev.js
@@ -1,27 +1,27 @@
-const esbuild = require("esbuild");
-const packagejson = require("./package.json");
+const esbuild = require('esbuild');
+const packagejson = require('./package.json');
 
 const sharedConfig = {
   loader: {
-    ".tsx": "tsx",
-    ".ts": "tsx",
+    '.tsx': 'tsx',
+    '.ts': 'tsx',
   },
-  outbase: "./src",
+  outbase: './src',
   bundle: true,
-  jsxFactory: "createElement",
-  jsxFragment: "Fragment",
-  target: ["esnext"],
-  logLevel: "debug",
+  jsxFactory: 'createElement',
+  jsxFragment: 'Fragment',
+  target: ['esnext'],
+  logLevel: 'debug',
   external: [...Object.keys(packagejson.peerDependencies || {})],
 };
 
 esbuild
   .build({
     ...sharedConfig,
-    entryPoints: ["src/index.ts"],
-    outdir: "dist/esm",
+    entryPoints: ['src/index.ts'],
+    outdir: 'dist/esm',
     sourcemap: true,
-    format: "esm",
+    format: 'esm',
     watch: true,
     banner: {
       js: "import { createElement, Fragment } from 'react';\n",
