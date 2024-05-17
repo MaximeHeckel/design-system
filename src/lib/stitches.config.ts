cc00ecfb28e93f8f82d85a53ee4884328fae90d1
Maxime <heckelmaxime@gmail.com>
2024-05-16 20:28:31 -0400

    style: Add standard breakpoints

--- src/lib/stitches.config.ts
+++ src/lib/stitches.config.ts
@@ -1,7 +1,15 @@
 import { createStitches, CSS as StitchesCSS } from '@stitches/react';
 
 const { config, css, getCssText, globalCss, keyframes, styled, theme } =
-  createStitches();
+  createStitches({
+    media: {
+      xs: '(min-width: 480px)',
+      sm: '(min-width: 640px)',
+      md: '(min-width: 768px)',
+      lg: '(min-width: 1024px)',
+      xl: '(min-width: 1280px)',
+    },
+  });
 
 export type CSS = StitchesCSS<typeof config>;
 export type { VariantProps } from '@stitches/react';
