46511ccc115c3301a5bc86d048de05a4f6f7b614
Maxime <heckelmaxime@gmail.com>
2024-11-11 15:40:25 -0500

    fix: Fix invalid react prop in bluesky icon

--- src/components/Icon/IconList.tsx
+++ src/components/Icon/IconList.tsx
@@ -27,12 +27,12 @@ export const BlueskyIcon = (props: IconProps) => (
   <StyledSVG
     stroke="currentColor"
     fill="none"
-    stroke-width="2"
+    strokeWidth="2"
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     role="img"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
@@ -61,25 +61,6 @@ export const BlueskyIcon = (props: IconProps) => (
       strokeLinejoin="round"
     />
   </StyledSVG>
-  // <StyledSVG
-  //   xmlns="http://www.w3.org/2000/svg"
-  //   width="22"
-  //   height="22"
-  //   viewBox="0 0 25 24"
-  //   stroke="currentColor"
-  //   strokeWidth="2"
-  //   role="img"
-  //   {...props}
-  // >
-  //   <title>Bluesky</title>
-  //   <desc>The outline of a blue bird, the logo of bsky.app</desc>
-  //   <path
-  //     d="M23.8618 2.9995C22.9042 3.67497 21.8439 4.19161 20.7218 4.5295C20.1196 3.83701 19.3192 3.34619 18.4289 3.12342C17.5386 2.90066 16.6013 2.95669 15.7439 3.28395C14.8865 3.61121 14.1503 4.1939 13.6348 4.95321C13.1193 5.71253 12.8495 6.61183 12.8618 7.5295V8.5295C11.1044 8.57506 9.36309 8.18531 7.79283 7.39494C6.22256 6.60458 4.87213 5.43813 3.86182 3.9995C3.86182 3.9995 -0.138184 12.9995 8.86182 16.9995C6.80234 18.3975 4.34897 19.0984 1.86182 18.9995C10.8618 23.9995 21.7818 18.8949 21.7818 7.39494C21.7809 7.1164 21.8341 6.94309 21.7818 6.6695C22.8024 5.66299 23.5226 4.39221 23.8618 2.9995Z"
-  //     role="presentation"
-  //     strokeLinecap="round"
-  //     strokeLinejoin="round"
-  //   />
-  // </StyledSVG>
 );
 
 export const GithubIcon = (props: IconProps) => (
