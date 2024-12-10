3943e014ee3570cdf0091433afefa92029187dff
Maxime <heckelmaxime@gmail.com>
2024-12-09 20:41:07 -0500

    chore: Remove framer-motion peer dependency

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,13 +1,7 @@
-import { ForwardRefComponent } from 'framer-motion';
-
 export type MainButtonVariant = 'primary' | 'secondary';
 export type IconButtonVariant = 'icon';
 
 interface BaseButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
-  // Note @MaximeHeckel: I honestly gave up on typing this properly
-  // Originally I wanted to type it as ForwardRefComponent<HTMLButtonElement, HTMLMotionProps<"button">>;
-  // But that is somehow invalid :shurg. The objective here would be to only allow polymorphism for motion.button
-  as?: ForwardRefComponent<HTMLButtonElement, any>;
   type?: 'button' | 'reset' | 'submit';
 }
 
