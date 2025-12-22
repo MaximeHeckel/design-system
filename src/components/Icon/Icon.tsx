ffcf3e89c76e75102b17184ba858a47b9bef5b37
Maxime Heckel <heckelmaxime@gmail.com>
2025-12-22 16:40:35 +0100

    feat: New icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -15,10 +15,20 @@ import {
   GithubIcon,
   MapIcon,
   StackIcon,
-  XIcon,
   ChevronIcon,
   FullScreenIcon,
   ExternalLinkIcon,
+  ClearIcon,
+  PiPIcon,
+  PiPExitIcon,
+  VolumeUpIcon,
+  VolumeDownIcon,
+  VolumeMuteIcon,
+  CodeIcon,
+  CopyIcon,
+  CoffeeIcon,
+  AIChatIcon,
+  CommandIcon,
 } from './IconList';
 
 const Icon = () => {};
@@ -39,10 +49,21 @@ Icon.Alert = AlertIcon;
 Icon.Github = GithubIcon;
 Icon.Map = MapIcon;
 Icon.Stack = StackIcon;
-Icon.X = XIcon;
+Icon.X = ClearIcon;
 Icon.Chevron = ChevronIcon;
 Icon.FullScreen = FullScreenIcon;
 Icon.ExternalLink = ExternalLinkIcon;
+Icon.Clear = ClearIcon;
+Icon.PiP = PiPIcon;
+Icon.PiPExit = PiPExitIcon;
+Icon.VolumeUp = VolumeUpIcon;
+Icon.VolumeDown = VolumeDownIcon;
+Icon.VolumeMute = VolumeMuteIcon;
+Icon.Code = CodeIcon;
+Icon.Copy = CopyIcon;
+Icon.Coffee = CoffeeIcon;
+Icon.Command = CommandIcon;
+Icon.AIChat = AIChatIcon;
 Icon.displayName = 'Icon';
 
 export default Icon;
